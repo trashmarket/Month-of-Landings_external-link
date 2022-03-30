e1ebb9071106519c3fcfe80f4c84220b1232be08
Nikita <prey122@gmail.com>
2022-03-30 17:01:13 +0300

    adaptive header menu tablet done

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const burgerButton = document.querySelector('.header__burger-button');
 const menu = document.querySelector('.header__menu-wrapper');
+const headerLogo = document.querySelector('.header__logo');
 
 function chengeClassList (nameFordel, nameForAdd) {
   burgerButton.classList.remove(nameFordel);
@@ -13,4 +14,12 @@ burgerButton.onclick = function(event) {
     chengeClassList('header__burger-button_type_open', 'header__burger-button_type_close');
   }
   menu.classList.toggle('header__menu-wrapper_active');
+
+  if (window.innerWidth > 767) {
+    if (!headerLogo.classList.contains('header__logo_hide')) {
+      headerLogo.classList.add('header__logo_hide');
+    } else {
+      headerLogo.classList.remove('header__logo_hide');
+    }
+  }
 }
